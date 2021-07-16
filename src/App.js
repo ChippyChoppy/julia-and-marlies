07618da82ccda3877e55324697c5b0860ca37776
ChippyChoppy <rebeccamargaret13@gmail.com>
2021-07-16 01:30:39 -0400

    getting going

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
   return (
     <div className="App">
-      <h1>Coming Soon!</h1>
+      <p> test </p>
     </div>
   );
 }
