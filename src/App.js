fb3ee8dcbdf7e18642491b3b4d8e23e4ed7bc411
ChippyChoppy <rebeccamargaret13@gmail.com>
2021-08-17 21:46:59 -0400

    coming soon w/ individual pieces

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
 import './App.css';
+import banner from './ComingSoonBanner.svg';
+import nameDate from './JandMwithDate.svg';
+import styled from 'styled-components';
 
+const NameDate = styled.img `
+@media(max-width: 500px) {
+  width: 90%;
+}
+  width: 69%;
+  padding: 6% 0% 3% 2%;`
+
+const ComingSoon = styled.img `
+@media (max-width: 500px) {
+  max-width: 75%;
+}
+  height: 64%;
+  padding: 0% 3% 0% 0%;
+`
 
 function App() {
   return (
     <div className="App">
+      <NameDate src={nameDate} alt="Julia And Marlies, June 25, 2022" />
+      <ComingSoon src={banner} alt="Celebration information coming soon!" />
     </div>
   );
 }
